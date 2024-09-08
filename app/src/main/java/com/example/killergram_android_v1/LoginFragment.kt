62022291fd89a5cc102cff813f1c7cf8015589bb
uson1004 <yoonbk2001027@gmail.com>
2024-09-08 20:36:19 +0900

    feat :: SignUpFragment View 구성

--- app/src/main/java/com/example/killergram_android_v1/LoginFragment.kt
+++ app/src/main/java/com/example/killergram_android_v1/LoginFragment.kt
@@ -1,18 +1,69 @@
 package com.example.killergram_android_v1
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.killergram_android_v1.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.inputPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s != null) {
+                    when {
+                        s.isEmpty() -> {
+                            binding.inputPassword.error = "비밀번호를 입력해주세요."
+                            Log.d("TEST", "비밀번호를 입력해주세요.")
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+    fun passwordRegex(password: String): Boolean {
+        return password.matches("^(?=.*[A-Za-z])(?=.*)".toRegex())
+    }
+
+    private val passwordListener = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+
+        }
     }
 }
