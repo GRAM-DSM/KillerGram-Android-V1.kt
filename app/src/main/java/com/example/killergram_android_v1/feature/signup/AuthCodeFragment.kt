c14750a27c623237b4cae7a15a65ea9132bf884c
유섭 <yoonbk2001027@gmail.com>
2024-09-11 08:13:54 +0900

    feat :: 회원가입 퍼블리싱

--- app/src/main/java/com/example/killergram_android_v1/feature/signup/AuthCodeFragment.kt
+++ app/src/main/java/com/example/killergram_android_v1/feature/signup/AuthCodeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.killergram_android_v1.feature.signup
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,8 +17,12 @@ class AuthCodeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_email_verification, container, false)
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+
+    }
 }
