173b87cf0ec8ced4f4803cf67a3fdf57d6d9f144
유섭 <yoonbk2001027@gmail.com>
2024-09-11 08:13:54 +0900

    feat :: 회원가입 퍼블리싱

--- app/src/main/java/com/example/killergram_android_v1/SignUpFragment.kt
+++ app/src/main/java/com/example/killergram_android_v1/SignUpFragment.kt
@@ -1,18 +1,66 @@
 package com.example.killergram_android_v1
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.killergram_android_v1.databinding.FragmentEmailVerificationBinding
 
 class SignUpFragment : Fragment() {
+
+    private val binding by lazy {
+        FragmentEmailVerificationBinding.inflate(layoutInflater)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        onInputTextChanged()
+        onInputFocusChanged()
+
+
+    }
+
+    private fun onInputTextChanged() {
+        binding.inputEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s?.length!! >= binding.inputEmailLayout.getCounterMaxLength())
+                    binding.inputEmailLayout.setError("Max character length is " + binding.inputEmailLayout.getCounterMaxLength());
+                else
+                    binding.inputEmailLayout.setError(null);
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+    }
+
+    private fun onInputFocusChanged() {
+        binding.inputEmail.onFocusChangeListener = View.OnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                binding.btnLogin.setBackgroundResource(R.drawable.bg_button_radius_0)
+            } else {
+                binding.btnLogin.setBackgroundResource(R.drawable.bg_button_radius_gray)
+            }
+        }
+    }
+
+    private fun fragmentTransaction() {
+
     }
 }
