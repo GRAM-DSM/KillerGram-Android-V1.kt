15e1565d65f9b72d8d36c3bb2405d4e01a34603d
유섭 <yoonbk2001027@gmail.com>
2024-09-11 08:13:54 +0900

    feat :: 회원가입 퍼블리싱

--- app/src/main/java/com/example/killergram_android_v1/feature/signup/AuthCodeFragment.kt
+++ app/src/main/java/com/example/killergram_android_v1/feature/signup/AuthCodeFragment.kt
@@ -3,26 +3,79 @@ package com.example.killergram_android_v1.feature.signup
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+<<<<<<<< HEAD:app/src/main/java/com/example/killergram_android_v1/feature/signup/AuthCodeFragment.kt
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.killergram_android_v1.R
+========
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.killergram_android_v1.databinding.FragmentEmailVerificationBinding
+>>>>>>>> 173b87c (feat :: 회원가입 퍼블리싱):app/src/main/java/com/example/killergram_android_v1/SignUpFragment.kt
 
 
 class AuthCodeFragment : Fragment() {
+
+    private val binding by lazy {
+        FragmentEmailVerificationBinding.inflate(layoutInflater)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+<<<<<<<< HEAD:app/src/main/java/com/example/killergram_android_v1/feature/signup/AuthCodeFragment.kt
 
     }
 }
+========
+        onInputTextChanged()
+        onInputFocusChanged()
+
+
+    }
+
+    private fun onInputTextChanged() {
+        binding.inputEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s?.length!! >= binding.inputEmailLayout.getCounterMaxLength())
+                    binding.inputEmailLayout.setError("Max character length is " + binding.inputEmailLayout.getCounterMaxLength());
+                else
+                    binding.inputEmailLayout.setError(null);
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+    }
+
+    private fun onInputFocusChanged() {
+        binding.inputEmail.onFocusChangeListener = View.OnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                binding.btnLogin.setBackgroundResource(R.drawable.bg_button_radius_0)
+            } else {
+                binding.btnLogin.setBackgroundResource(R.drawable.bg_button_radius_gray)
+            }
+        }
+    }
+
+    private fun fragmentTransaction() {
+
+    }
+}
+>>>>>>>> 173b87c (feat :: 회원가입 퍼블리싱):app/src/main/java/com/example/killergram_android_v1/SignUpFragment.kt
