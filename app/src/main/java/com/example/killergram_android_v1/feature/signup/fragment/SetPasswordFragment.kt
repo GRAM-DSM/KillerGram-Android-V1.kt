7dbfa4d65ae91d5bd6457e065e166f2b9623a58c
유섭 <yoonbk2001027@gmail.com>
2024-09-11 23:15:46 +0900

    chore :: 추가하지 못한 파일들 생성

---
+++ app/src/main/java/com/example/killergram_android_v1/feature/signup/fragment/SetPasswordFragment.kt
@@ -0,0 +1,26 @@
+package com.example.killergram_android_v1.feature.signup.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.killergram_android_v1.R
+
+
+class AuthCodeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_auth_code, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
