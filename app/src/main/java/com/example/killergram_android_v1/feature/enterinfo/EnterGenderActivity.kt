c34281d050d6321c3b556d8ab0c125e8e3dbfd89
유섭 <yoonbk2001027@gmail.com>
2024-09-19 10:56:15 +0900

    feat :: EnterGender 뷰 추가

--- app/src/main/java/com/example/killergram_android_v1/feature/enterinfo/EnterGenderActivity.kt
+++ app/src/main/java/com/example/killergram_android_v1/feature/enterinfo/EnterGenderActivity.kt
@@ -3,13 +3,15 @@ package com.example.killergram_android_v1.feature.enterinfo
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.killergram_android_v1.R
 
-class EnterNameActivity : AppCompatActivity() {
+class EnterGenderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_enter_name)
+        setContentView(R.layout.activity_enter_gender)
 
     }
 }
