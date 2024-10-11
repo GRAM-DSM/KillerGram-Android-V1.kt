8f9a365bb98a9fdc16b7839a547739dcbd26c5ea
유섭 <yoonbk2001027@gmail.com>
2024-10-11 16:10:44 +0900

    feat :: homeActivity 오늘 날짜 구현

--- app/src/main/java/com/example/killergram_android_v1/feature/home/HomeActivity.kt
+++ app/src/main/java/com/example/killergram_android_v1/feature/home/HomeActivity.kt
@@ -9,6 +9,7 @@ import com.example.killergram_android_v1.R
 import com.example.killergram_android_v1.databinding.ActivityHomeBinding
 import java.text.SimpleDateFormat
 import java.time.DayOfWeek
+import java.time.LocalDate
 import java.util.Calendar
 import java.util.Date
 import java.util.Locale
@@ -26,11 +27,32 @@ class HomeActivity : AppCompatActivity() {
 
     }
 
-    private fun getDate()  { // 요일, 날짜
-        val now =  System.currentTimeMillis()
-        val simpleDateFormat = SimpleDateFormat("EEEE", Locale.KOREA).format(now)
-        val simpleDateFormatDate = SimpleDateFormat("dd", Locale.KOREA).format(now)
-        //val date: String = Date(simpleDateFormat, simpleDateFormatDate).toString()
+    private fun getDate() {
+        var now = LocalDate.now()
+        val weeks = DayOfWeek.entries.toList()
 
+        val days = mutableListOf<Int>()
+
+        when (now.dayOfWeek) {
+            in DayOfWeek.MONDAY..DayOfWeek.FRIDAY -> {
+                now = now.minusDays(weeks.indexOf(now.dayOfWeek).toLong())
+            }
+
+            DayOfWeek.SATURDAY -> {
+                now = now.plusDays(2)
+            }
+
+            DayOfWeek.SUNDAY -> {
+                now = now.plusDays(1)
+            }
+
+            else -> {}
+        }
+
+        repeat(5) {
+            now = now.plusDays(1)
+            days.add(now.dayOfMonth - 1)
+            binding.tvDateFirst.setText(now.toString()).toString()
+        }
     }
 }
