561c963f0cf7c44b316751734e866c2ea6228b09
유섭 <yoonbk2001027@gmail.com>
2024-10-15 22:06:10 +0900

    feat :: chnageSkillLevel button click 화면전환 구현

--- app/src/main/java/com/example/killergram_android_v1/feature/submitlist/changeSkillLevelActivity.kt
+++ app/src/main/java/com/example/killergram_android_v1/feature/submitlist/changeSkillLevelActivity.kt
@@ -1,17 +1,33 @@
 package com.example.killergram_android_v1.feature.submitlist
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.killergram_android_v1.R
+import com.example.killergram_android_v1.databinding.ActivityChangeSkillLevelBinding
 
-class changeSkillLevelActivity : AppCompatActivity() {
+class ChangeSkillLevelActivity : AppCompatActivity(), View.OnClickListener {
+    private val binding by lazy {
+        ActivityChangeSkillLevelBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_change_skill_level)
+        setContentView(binding.root)
 
+        binding.btnChangeSkillSubmit.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val changeSkillLevelToSubmit = Intent(this, SubmitBasketballActivity::class.java)
+
+        when(v?.id) {
+            R.id.btn_change_skill_submit -> {
+                startActivity(changeSkillLevelToSubmit)
+            }
+        }
     }
 }
