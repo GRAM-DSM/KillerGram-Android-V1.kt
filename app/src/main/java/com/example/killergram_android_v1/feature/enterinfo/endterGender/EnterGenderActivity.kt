df60fb59ba50ee087ce01d6155b24dff3a8fba39
유섭 <yoonbk2001027@gmail.com>
2024-10-15 23:06:04 +0900

    feat :: EnterGenderViewModel view selected 연동

--- app/src/main/java/com/example/killergram_android_v1/feature/enterinfo/endterGender/EnterGenderActivity.kt
+++ app/src/main/java/com/example/killergram_android_v1/feature/enterinfo/endterGender/EnterGenderActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.lifecycle.ViewModelProvider
 import com.example.killergram_android_v1.R
 import com.example.killergram_android_v1.databinding.ActivityEnterGenderBinding
 import com.example.killergram_android_v1.feature.enterinfo.EnterGradeActivity
@@ -14,6 +16,9 @@ class EnterGenderActivity : AppCompatActivity(), View.OnClickListener {
     private val binding by lazy {
         ActivityEnterGenderBinding.inflate(layoutInflater)
     }
+    private val enterGenderViewModel by lazy {
+        ViewModelProvider(this@EnterGenderActivity)[EnterGenderViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,16 +27,15 @@ class EnterGenderActivity : AppCompatActivity(), View.OnClickListener {
 
         binding.imgLeftArrow.setOnClickListener(this)
         binding.btnLogin.setOnClickListener(this)
-
         binding.btnMan.setOnClickListener(this)
+        binding.btnWoman.setOnClickListener(this)
+
+        onGenderToggleSelected()
     }
 
     override fun onClick(v: View?) {
         val enterGenderToEnterSkills = Intent(this, EnterSkillsActivity::class.java)
         val enterGenderToEnterGrade = Intent(this, EnterGradeActivity::class.java)
-
-        var selectW = false
-        var selectM = false
 
         when (v?.id) {
             R.id.btn_login -> {
@@ -43,19 +47,27 @@ class EnterGenderActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             R.id.btn_man -> {
-                onWomanToggleSelected()
+                enterGenderViewModel.onSelectItem(1)
             }
 
             R.id.btn_woman -> {
-                onManToggleSelected()
+                enterGenderViewModel.onSelectItem(2)
             }
         }
     }
 
-    private fun onWomanToggleSelected() {
-    }
-
-    private fun onManToggleSelected() {
-
+    private fun onGenderToggleSelected() {
+        enterGenderViewModel.buttonState.observe(this@EnterGenderActivity) {
+            when(it) {
+                1 -> {
+                    binding.btnMan.background = AppCompatResources.getDrawable(this, R.drawable.button_selected)
+                    binding.btnWoman.background = AppCompatResources.getDrawable(this, R.drawable.button_unselected)
+                }
+                2 -> {
+                    binding.btnMan.background = AppCompatResources.getDrawable(this, R.drawable.button_unselected)
+                    binding.btnWoman.background = AppCompatResources.getDrawable(this, R.drawable.button_selected)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/killergram_android_v1/feature/enterinfo/endterGender/EnterGenderViewModel.kt
+++ app/src/main/java/com/example/killergram_android_v1/feature/enterinfo/endterGender/EnterGenderViewModel.kt
@@ -1,5 +1,14 @@
 package com.example.killergram_android_v1.feature.enterinfo.endterGender
 
-class EnterGenderViewModel {
-    
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class EnterGenderViewModel: ViewModel() {
+    private val _buttonState: MutableLiveData<Int> = MutableLiveData() // setter
+    var buttonState: LiveData<Int> = _buttonState // getter
+
+    fun onSelectItem(selectedButton: Int) {
+        _buttonState.value = selectedButton
+    }
 }
