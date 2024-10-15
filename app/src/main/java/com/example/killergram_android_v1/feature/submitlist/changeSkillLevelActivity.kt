436e33853e198756ab2ff0139716223bdde888e3
유섭 <yoonbk2001027@gmail.com>
2024-10-15 21:52:42 +0900

    feat :: change skill level 퍼블리싱

---
+++ app/src/main/java/com/example/killergram_android_v1/feature/submitlist/changeSkillLevelActivity.kt
@@ -0,0 +1,17 @@
+package com.example.killergram_android_v1.feature.submitlist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.killergram_android_v1.R
+
+class changeSkillLevelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_change_skill_level)
+
+    }
+}
