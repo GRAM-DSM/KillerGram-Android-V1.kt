5c1f49f55b3cffe3685259bc46ae11f3bbef0393
유섭 <yoonbk2001027@gmail.com>
2024-10-15 21:45:46 +0900

    feat :: 이메일 인증 View timer 기능 구현

--- app/src/main/java/com/example/killergram_android_v1/feature/signup/EmailValidationActivity.kt
+++ app/src/main/java/com/example/killergram_android_v1/feature/signup/EmailValidationActivity.kt
@@ -7,11 +7,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.killergram_android_v1.R
 import com.example.killergram_android_v1.databinding.ActivityEmailValidationBinding
+import java.util.Objects
+import java.util.Timer
+import java.util.TimerTask
 
 class EmailValidationActivity : AppCompatActivity(), View.OnClickListener {
     private val binding by lazy {
         ActivityEmailValidationBinding.inflate(layoutInflater)
     }
+    private var time = 0
+    private val timer = Timer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,7 @@ class EmailValidationActivity : AppCompatActivity(), View.OnClickListener {
 
         binding.btnLogin.setOnClickListener(this)
         binding.imgLeftArrow.setOnClickListener(this)
+        binding.tvEmailReverification.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
@@ -32,6 +38,29 @@ class EmailValidationActivity : AppCompatActivity(), View.OnClickListener {
             }
             R.id.img_left_arrow -> {
                 startActivity(emailValidationToInputEmail)
+            }
+            R.id.tv_email_reverification -> {
+                timer.schedule(getTaskTimer(), 1000, 1000)
+            }
+        }
+    }
+
+    private fun getTaskTimer(): TimerTask {
+        binding.tvTitleTimer.text = "05:00"
+        time = 300
+
+        return object : TimerTask() {
+            override fun run() {
+                runOnUiThread {
+                    if (time == 0) {
+                        timer.cancel()
+                        binding.tilEmailVerification.error = "제한시간 5분을 초과하였습니다."
+                    }
+                    time -= 1
+                    val min = time / 60
+                    val sec = time % 60
+                    binding.tvTitleTimer.text = String.format("%02d:%02d", min, sec)
+                }
             }
         }
     }
