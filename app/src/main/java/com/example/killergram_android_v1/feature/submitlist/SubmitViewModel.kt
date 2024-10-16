bd8dbd05cd1f1f4d255a6ec2208e5c930f47e87c
유섭 <yoonbk2001027@gmail.com>
2024-10-16 13:31:34 +0900

    style :: text bold 제거

--- app/src/main/java/com/example/killergram_android_v1/feature/submitlist/SubmitViewModel.kt
+++ app/src/main/java/com/example/killergram_android_v1/feature/submitlist/SubmitViewModel.kt
@@ -8,14 +8,7 @@ class SubmitViewModel: ViewModel() {
     private val _buttonState: MutableLiveData<Int> = MutableLiveData() // setter
     var buttonState: LiveData<Int> = _buttonState // getter
 
-    private val _textState: MutableLiveData<String> = MutableLiveData()
-    var textState: LiveData<String> = _textState
-
     fun onSelectItem(selectedButton: Int) {
         _buttonState.value = selectedButton
-    }
-
-    fun setLiveText(change: String) {
-        _textState.value = change
     }
 }
