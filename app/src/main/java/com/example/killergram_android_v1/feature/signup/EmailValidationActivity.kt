7d33e9ee75c80ef6aa25ca6df7c39438e2a6a6aa
유섭 <yoonbk2001027@gmail.com>
2024-11-05 17:35:12 +0900

    feat :: email 값 전송 인스턴스 로직 생성

--- app/src/main/java/com/example/killergram_android_v1/feature/signup/EmailValidationActivity.kt
+++ app/src/main/java/com/example/killergram_android_v1/feature/signup/EmailValidationActivity.kt
@@ -3,10 +3,17 @@ package com.example.killergram_android_v1.feature.signup
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.killergram_android_v1.R
+import com.example.killergram_android_v1.data.api.ApiProvider
+import com.example.killergram_android_v1.data.request.auth.EmailVerificationRequest
+import com.example.killergram_android_v1.data.response.auth.EmailVerificationResponse
 import com.example.killergram_android_v1.databinding.ActivityEmailValidationBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.Objects
 import java.util.Timer
 import java.util.TimerTask
@@ -17,6 +24,8 @@ class EmailValidationActivity : AppCompatActivity(), View.OnClickListener {
     }
     private var time = 0
     private val timer = Timer()
+
+    private val retrofit = ApiProvider.getAuthApi()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +38,13 @@ class EmailValidationActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(view: View?) {
-        val emailValidationToSetPassword = Intent(this, SetPasswordActivity::class.java)
         val emailValidationToInputEmail = Intent(this, InputEmailActivity::class.java)
+
+        val email = binding.tieEmailVerification.text.toString()
 
         when(view?.id) {
             R.id.btn_login -> {
-                startActivity(emailValidationToSetPassword)
+                connectEmailToServer(email)
             }
             R.id.img_left_arrow -> {
                 startActivity(emailValidationToInputEmail)
@@ -63,5 +73,27 @@ class EmailValidationActivity : AppCompatActivity(), View.OnClickListener {
                 }
             }
         }
+    }
+
+    private fun connectEmailToServer(email: String) {
+        val emailValidationToSetPassword = Intent(this, SetPasswordActivity::class.java)
+
+        retrofit.emailVerification(
+            EmailVerificationRequest(
+                email = email
+            )
+        ).enqueue(object : Callback<EmailVerificationResponse> {
+            override fun onResponse(
+                call: Call<EmailVerificationResponse>,
+                response: Response<EmailVerificationResponse>
+            ) {
+                Toast.makeText(baseContext, "이메일 값 성공!", Toast.LENGTH_SHORT).show()
+                startActivity(emailValidationToSetPassword)
+            }
+
+            override fun onFailure(call: Call<EmailVerificationResponse>, t: Throwable) {
+
+            }
+        })
     }
 }
