c12ceff6c7a8c89e0dbb1e2ba6305eed08a3d620
유섭 <yoonbk2001027@gmail.com>
2024-11-12 09:10:22 +0900

    feat :: firebase messaging(FCM) 클래스 생성

---
+++ app/src/main/java/com/example/killergram_android_v1/MyFirebaseMessagingService.kt
@@ -0,0 +1,7 @@
+package com.example.killergram_android_v1
+
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+}
