beecfb144f67a563a0f0e01ce6eb1870ae799122
유섭 <yoonbk2001027@gmail.com>
2024-11-13 23:00:01 +0900

    feat :: recycler view adapter filter 적용

--- app/src/main/java/com/example/killergram_android_v1/feature/recyclerView/home/HomeAdapter.kt
+++ app/src/main/java/com/example/killergram_android_v1/feature/recyclerView/home/HomeAdapter.kt
@@ -1,29 +1,38 @@
 package com.example.killergram_android_v1.feature.recyclerView.home
 
 import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.example.killergram_android_v1.R
 import com.example.killergram_android_v1.databinding.ListItemBinding
 import com.example.killergram_android_v1.feature.recyclerView.home.data.Sport
 
 class HomeAdapter(
-    private val items: List<Sport>,
-    private val itemClickListener: (Int) -> Unit
-) : RecyclerView.Adapter<HomeAdapter.Holder>() {
+    private val items: MutableList<Sport>,
+    private val itemClickListener: (Int) -> Unit,
+    unFilteredList: ArrayList<String>,
+) : RecyclerView.Adapter<HomeAdapter.Holder>(), Filterable {
+
+    companion object {
+        // TODO: fullList
+        private val fullList = arrayListOf<String>()
+    }
+
+    // TODO: filteredList
+    private var filteredList = ArrayList<String>()
+
+    init {
+        // 초기화 시 fullList와 filteredList를 unFilteredList로 설정
+        fullList.addAll(unFilteredList)
+        filteredList.addAll(unFilteredList)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val binding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-
-        val layoutParams = binding.root.layoutParams as RecyclerView.LayoutParams
-
-    //    val marginInDp = 16
-    //    val marginInPx = dpToPx(parent.context, marginInDp)
-    //    layoutParams.setMargins(0, marginInPx, 0, 0)
-    //
-    //    binding.root.layoutParams = layoutParams
-
 
         return Holder(binding, itemClickListener)
     }
@@ -34,6 +43,33 @@ class HomeAdapter(
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         holder.bind(items[position])
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charString = constraint?.toString() ?: ""
+                filteredList = if (charString.isEmpty()) {
+                    ArrayList(null)  // TODO: fullList를 기반으로 필터링이 없을 때 존재하지 않는 리스트(null or "")를 반환
+                } else {
+                    // TODO: filteringList
+                    val filteringList = ArrayList<String>()
+                    for (name in fullList) { // 항상 fullList에서 필터링
+                        if (name.lowercase().contains(charString.lowercase())) {
+                            filteringList.add(name)
+                        }
+                    }
+                    filteringList
+                }
+                return FilterResults().apply { values = filteredList }
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                filteredList = results?.values as ArrayList<String>
+                Log.d("TEST", "Filtered List: $filteredList")
+                notifyDataSetChanged()
+            }
+        }
     }
 
     class Holder(
