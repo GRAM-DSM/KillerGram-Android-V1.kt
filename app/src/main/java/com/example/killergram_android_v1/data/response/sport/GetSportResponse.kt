5455803b6960dceec31a60fbdb2bb7b32155f73f
유섭 <yoonbk2001027@gmail.com>
2024-11-15 15:31:52 +0900

    style :: getSport response 값 변경

--- app/src/main/java/com/example/killergram_android_v1/data/response/sport/GetSportResponse.kt
+++ app/src/main/java/com/example/killergram_android_v1/data/response/sport/GetSportResponse.kt
@@ -2,11 +2,14 @@ package com.example.killergram_android_v1.data.response.sport
 
 import com.google.gson.annotations.SerializedName
 
-data class GetSportResponse(
-    @SerializedName("created_date") val createdDate: String,
-    @SerializedName("manager_email") val managerEmail: String,
+data class GetSportResponsee(
+    @SerializedName("sport_id") val sport_id: String,
+    @SerializedName("manager_email") val manager_email: String,
+    @SerializedName("sport_name") val sport_name: String,
     @SerializedName("personnel") val personnel: Int,
+    @SerializedName("current_personnel") val current_personnel: Int,
+    @SerializedName("created_date") val created_date: String,
     @SerializedName("position") val position: Boolean,
-    @SerializedName("sport_id") val sportId: String,
-    @SerializedName("sport_name") val sportName: String
+    
+    @SerializedName("enabled") val enabled: Boolean,
 )
